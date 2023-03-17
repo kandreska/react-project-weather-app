c2d92054a14d74d490060dc98c242782fbc42014
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-17 10:23:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project is coded by Kristin Andreka and is{" "} 
+        This project is coded by Kristin Andreka and is{" "}
         <a
           href="https://github.com/kandreska/react-project-weather-app"
           target="_blank"
