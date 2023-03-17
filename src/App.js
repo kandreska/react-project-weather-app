3ba0ab84e5c087fe909cd5ccb233ea834a319252
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-17 10:17:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Kristin Andreka and is{" "} 
+        <a
+          href="https://github.com/kandreska/react-project-weather-app"
+          target="_blank"
+        >
+          Open-Sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
