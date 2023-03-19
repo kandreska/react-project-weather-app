814fa4d05b71411b62aae8609859aabbac0e1d6e
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-18 21:18:03 -0500

    Edits

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather defaultCity="Dallas" />
+
         <footer>
           This project is coded by Kristin Andreska and is{" "}
           <a
