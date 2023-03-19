301a51897a4fa63d8832ce1b514e293752dae8be
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-19 12:19:44 -0500

    Change to Imperial Temperature

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,7 +3,7 @@ import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
@@ -37,7 +37,7 @@ export default function WeatherForecast() {
      let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
      let longitude = props.coordinates.lon;
      let latitude = props.coordinates.lat;
-     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
      axios.get(apiUrl).then(handleResponse);
 
