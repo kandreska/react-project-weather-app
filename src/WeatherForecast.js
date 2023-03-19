e29f640543d80b32bff9f9badeac05fa43ed8132
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-19 12:33:36 -0500

    Edited Warnings

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,31 +16,36 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
-   if (loaded) {
-     return (
-       <div className="WeatherForecast">
-         <div className="row">
-           {forecast.map(function(dailyForecast, index) {
-             if (index < 5) {
-               return (
-                 <div className="col" key={index}>
-                   <WeatherForecastDay data={dailyForecast} />
-                 </div>
-               );
-             }
-           })}
-         </div>
-       </div>
-     );
+  function load() {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-   } else {
-     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-     let longitude = props.coordinates.lon;
-     let latitude = props.coordinates.lat;
-     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-     axios.get(apiUrl).then(handleResponse);
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function(dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
 
-     return null;
-   }
+    return null;
+  }
 }
